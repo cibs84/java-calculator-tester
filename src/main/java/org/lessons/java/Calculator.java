8753bcafe6d19a8fda9e98885b407b08332d89ba
cibs84 <d.tilesi@gmail.com>
2023-02-09 15:39:34 +0100

    Esercizio completato

--- src/main/java/org/lessons/java/Calculator.java
+++ src/main/java/org/lessons/java/Calculator.java
@@ -1,5 +1,19 @@
 package org.lessons.java;
 
 public class Calculator {
-	
+	public float add(float num1, float num2) {
+		return num1 + num2;
+	}
+
+	public float subtract(float num1, float num2) {
+		return num1 - num2;
+	}
+
+	public float divide(float num1, float num2) {
+		return num1 / num2;
+	}
+
+	public float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
 }
--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -1,19 +1,46 @@
 package org.lessons.java;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	
+	private static Calculator calculator;
+	private float result;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		calculator = new Calculator();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("verifica metodo add()") 
+	void testAdd() {
+		result = calculator.add(6,3);
+		assertEquals(9, result, "Il risultato deve essere 9");
 	}
-
+	
+	@Test
+	@DisplayName("verifica metodo subtract()") 
+	void testSubtract() {
+		result = calculator.subtract(6,3);
+		assertEquals(3, result, "Il risultato deve essere 3");
+	}
+	
+	@Test
+	@DisplayName("verifica metodo divide()") 
+	void testDivide() {
+		result = calculator.divide(6,3);
+		assertEquals(2, result, "Il risultato deve essere 2");
+	}
+	
+	@Test
+	@DisplayName("verifica metodo multiply()") 
+	void testMultiply() {
+		result = calculator.multiply(6,3);
+		assertEquals(18, result, "Il risultato deve essere 18");
+	}
 }
